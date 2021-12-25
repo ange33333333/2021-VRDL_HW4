b638ea1b6d3347f75363337a3df67c0e7cddd5b1
ange33333333 <72069714+ange33333333@users.noreply.github.com>
2021-12-26 02:31:16 +0800

    Add files via upload

---
+++ scripts/flags.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-05-21 17:13
+# @Author  : LeeHW
+# @File    : flags.py
+# @Software: PyCharm
+train_HR_dir='C:/Users/ange3/OneDrive/Desktop/基於深度學習之視覺辦識專論/HW4/training_hr_images'
+save_dir='C:/Users/ange3/OneDrive/Desktop/基於深度學習之視覺辦識專論/HW4/training_lr_images'
